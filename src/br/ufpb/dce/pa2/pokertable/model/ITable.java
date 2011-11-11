93683fef1f8d5daaf6dc45e21e0f8910442b8460
Vicente Neto <vicente.neto@dce.ufpb.br>
2011-11-11 12:33:37 -0200

    Atualização da ITable com novos métodos e documentada.

--- src/br/ufpb/dce/pa2/pokertable/model/ITable.java
+++ src/br/ufpb/dce/pa2/pokertable/model/ITable.java
@@ -1,35 +1,127 @@
 package br.ufpb.dce.pa2.pokertable.model;
 
-public interface ITable {
-	public void setMinimumBet(int minimumBet);
+/**
+ * 
+ * @author Vicente Neto
+ *
+ */
+interface ITable {
+	/**
+	 * Sets the initial configuration of the table
+	 */
+	void begin();
 	
-	public int getDealerPosition();
+	/**
+	 * Sets the minimum bet of the table
+	 * @param minimumBet the minimum bet of the table
+	 */
+	void setMinimumBet(int minimumBet);
 	
-	public int getSmallBlindPosition();
+	/**
+	 * Returns the number of the Dealer position
+	 * @return An integer referring to the number of the Dealer position on the table
+	 */
+	int getDealerPosition();
 	
-	public int getBigBlindPosition();
+	/**
+	 * Returns the number of the Small Blinder position
+	 * @return An integer referring to the number of the Small Blinder position on the table
+	 */
+	int getSmallBlindPosition();
 	
-	public void passDealer();
+	/**
+	 * Returns the number of the Big Blinder position
+	 * @return An integer referring to the number of the Big Blinder position on the table
+	 */
+	int getBigBlindPosition();
 	
-	public void setDealerPostion(int dealerPosition);
+	/**
+	 * Change the Dealer to the next left player on the table
+	 */
+	void passDealer();
 	
-	public int getPot();
+	/**
+	 * Sets the position of the Dealer
+	 * @param dealerPosition An integer referring to the new position of the Dealer on the table
+	 */
+	void setDealerPostion(int dealerPosition);
 	
-	public int getSmallBlindBet();
+	/**
+	 * Returns the sum of all bets
+	 * @return The sum of all bets
+	 */
+	int getPot();
 	
-	public int getBigBlindBet();
+	/**
+	 * 
+	 * @return
+	 */
+	int getCurrentTurnPot();
 	
-	public int getActualBet();
+	/**
+	 * 
+	 */
+	void nextPlayer();
 	
-	public void bet(Player player, int value);
+	/**
+	 * Returns the minimum bet of the Small Blinder
+	 * @return The minimum bet of the Small Blinder
+	 */
+	int getSmallBlindBet();
 	
-	public void check(Player player);
+	/**
+	 * Returns the minimum bet of the Big Blinder
+	 * @return The minimum bet of the Big Blinder
+	 */
+	int getBigBlindBet();
 	
-	public void fold(Player player);
+	/**
+	 * Returns the sum of bet's on the current turn
+	 * @return The sum of bet's on the current turn
+	 */
+	int getCurrentTurnBet();
 	
-	public void call(Player player, int value);
+	/**
+	 * Returns the sum of player's bet in the current turn
+	 * @param player The player
+	 * @return The sum of bet's in the current turn
+	 */
+	int getCurrentTurnPlayerBet(Player player);
 	
-	public void begin();
+	/**
+	 * Increases the value of the bets on the current turn
+	 * @param player The player who increases the bet
+	 * @param value The value of the bet
+	 */
+	void bet(int value);
 	
-	public boolean EndOfTheGame();
+	/**
+	 * 
+	 * @param player 
+	 */
+	void check();
+	
+	/**
+	 * Removes a player from the rest of the match
+	 * @param player The player who will be removed from the match
+	 */
+	void fold();
+	
+	/**
+	 * 
+	 */
+	void nextTurn();
+	
+	/**
+	 * Increases the bet of a player in the current turn
+	 * @param player The player
+	 * @param value The value of the bet
+	 */
+	void call();
+	
+	/**
+	 * Returns a Boolean value to tell if the game is over or not
+	 * @return true if the game is over and False if the game is not over
+	 */
+	boolean EndOfTheGame();
 }
